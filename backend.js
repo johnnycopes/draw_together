241d8d449d296ac45ac4a31b937497a4e089b854
John Coppola <jdcoppola@gmail.com>
2016-12-09 14:41:18 -0500

    able to draw in two broswers

--- backend.js
+++ backend.js
@@ -3,8 +3,20 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+
+
 app.use(express.static('public'));
 
+// set up sockets on the back-end
+io.on('connection', function(socket) {
+  socket.broadcast.emit('draw');
+  console.log('server has connected!');
+  // 1) receive coordinates on the back-end
+  socket.on('draw coordinates', function(coords) {
+    // 2) send out coordinates to all clients on the front-end
+    socket.broadcast.emit('draw coordinates', coords);
+  });
+});
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
--- public/frontend.js
+++ public/frontend.js
@@ -1,5 +1,59 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+// set up sockets on the front-end
+const socket = io();
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// set up pen
+ctx.strokeStyle = 'cadetBlue';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+// drawing variables
+let isDrawing = false;
+let x1 = 0;
+let y1 = 0;
+
+canvas.addEventListener('mousedown', function(event) {
+  isDrawing = true;
+  // redefine x and y on every click of the mouse (the starting point of the line)
+  x1 = event.offsetX;
+  y1 = event.offsetY;
+  console.log(event);
+});
+
+
+canvas.addEventListener('mousemove', function(event) {
+  if (isDrawing) {
+    draw(x1, y1, event.offsetX, event.offsetY);
+    let coords = [x1, y1, event.offsetX, event.offsetY];
+      socket.emit('draw coordinates', coords);
+  }
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
+
+// 2) listen for incoming coordinates on the front-end
+socket.on('draw coordinates', function(coords){
+  // console.log(coords);\
+  draw(coords[0], coords[1], coords[2], coords[3]);
+});
+
+
+function draw(x1, y1, x2, y2) {
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(x1, y1);
+  //go to
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  // redefine x and y on every move of the mouse (every call of the 'draw' function)
+  var coords = [x1, y1, x2, y2];
+  x2 = x1;
+  y2 = y1;
+}
